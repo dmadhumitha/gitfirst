9aca9938dd8c4c03d42b77aea295709b3b0c5a74
MadhumithaDonthineni <madhumitha4412@gmail.com>
2023-03-09 14:36:06 -0600

    Output changes and new file added

--- Student.java
+++ Student.java
@@ -1,6 +1,7 @@
 public class Student{
     public static void main(String args[]){
 	  //Print Hello Class
-        System.out.println("Welcome to 1st day of class");
+        System.out.println("Hello World");
+        
     }
 }
